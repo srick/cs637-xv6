7f27792c21da697af034feb612d7ca681322225d
Alex Langenfeld <alangenfeld@wisc.edu>
2010-02-12 00:31:38 -0600

    benchmark here

--- tim.c
+++ tim.c
@@ -2,9 +2,40 @@
 #include "stat.h"
 #include "user.h"
 
+
+int
+spin(int n)
+{
+  int t0, tf;
+
+  volatile uint balls = 82345678;
+  t0 = tim();
+
+  while(balls--);
+  
+  tf = tim() - t0;
+   
+  printf(1, "Spinner %d: %d ticks\n",n, tf);
+
+  exit();
+}
+
 int
 main(int argc, char *argv[])
 {
-  printf(2, "%d\n", tim());
+  int kid1;
+
+  kid1 = fork();
+  
+  if(kid1 == 0)
+    spin(800);
+
+
+  if(kid1 > 0){
+    lotto(kid1, 800);
+    spin(100);
+  }
+  
   exit();
 }
+
