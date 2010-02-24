0a210d40da345b61daa4f4ea853b784059ac8ed5
Alex Langenfeld <alangenfeld@wisc.edu>
2010-02-23 21:02:16 -0600

    ya oh ya

--- thread.c
+++ thread.c
@@ -1,1 +1,26 @@
 #include "thread.h"
+#include "x86.h"
+
+int mutex_lock(mutex_t *m)
+{
+
+}
+
+int mutex_unlock(mutex_t *m)
+{
+
+
+} 
+
+int cond_wait(cond_t *c, mutex_t *m)
+{
+
+
+}
+
+int cond_signal(cond_t *c)
+{
+
+
+}
+
