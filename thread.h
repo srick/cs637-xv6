9d0c6e422f4545b7baafc22add0ce0e6b0ca7666
Alex Langenfeld <alangenfeld@wisc.edu>
2010-02-23 20:58:59 -0600

    Added thread primitives

--- thread.h
+++ thread.h
@@ -1,11 +1,3 @@
-/*
-#define thread_create(routine, arg)				\
-  void *sp = malloc(1024);					\
-  int pid = thread(sp);						\
-  if(!pid) {printf(1,"pid %d [%x]\n",pid, arg);routine(arg);}	\
-  else printf(1,"%d arg [%x]\n", pid, arg);
-*/
-
 static int 
 thread_create( void *(*start_routine)(void*), void *arg){
   void *sp = malloc(1024);
@@ -13,3 +5,20 @@ thread_create( void *(*start_routine)(void*), void *arg){
   if(!pid) (*start_routine)(arg);
   return pid;
 }
+
+struct mutex{
+  uint flag;
+};
+
+typedef struct mutex mutex_t;
+
+struct cond{
+  uint flag;
+};
+
+typedef struct cond cond_t;
+
+int mutex_lock(mutex_t *m);
+int mutex_unlock(mutex_t *m); 
+int cond_wait(cond_t *c, mutex_t *m);
+int cond_signal(cond_t *c);
